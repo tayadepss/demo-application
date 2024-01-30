0d704fd7162c799cdac36c42461e3883b798bf40
tayadepss <tayadepss@gmail.com>
2024-01-30 11:52:25 +0530

    first commit

---
+++ src/main/java/demoapplication/controller/DemoApplicationController.java
@@ -0,0 +1,18 @@
+package demoapplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/demo")
+public class DemoApplicationController {
+	
+	@GetMapping("hi")
+	public ResponseEntity<String> hi() {
+		return new ResponseEntity<String>("this is hi msg",HttpStatus.OK);
+	}	
+}
